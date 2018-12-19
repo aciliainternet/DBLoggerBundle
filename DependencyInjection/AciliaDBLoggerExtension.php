7637f4db123114763922221ad987ef31be2dc97f
Simon Berton <simonberton@gmail.com>
2018-12-19 15:28:21 -0300

    added configuration of bundle to container

--- DependencyInjection/AciliaDBLoggerExtension.php
+++ DependencyInjection/AciliaDBLoggerExtension.php
@@ -21,6 +21,8 @@ class AciliaDBLoggerExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
+        
+        $container->setParameter('acilia_dblogger', $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
